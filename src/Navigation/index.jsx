0e87d821c10fc7c5eb2cac0946e42c2ad2bf3b42
Martina Hytychová <martina.hyt@gmail.com>
2020-11-18 01:34:31 +0100

    feat: responsive design

--- src/Navigation/index.jsx
+++ src/Navigation/index.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './style.scss';
 
 export const Navigation = () => {
+  const [opened, setOpened] = useState(false);
+  const handleClick = () => {
+    setOpened(true);
+  };
   return (
     <>
       <nav className="navigation">
         <div className="navigation__logo">
-          <img src="https://fontmeme.com/permalink/201115/64b16bcedd83c388fc339a1662553d79.png" alt="logo"/>
+          <img
+            src="https://fontmeme.com/permalink/201115/64b16bcedd83c388fc339a1662553d79.png"
+            alt="logo"
+          />
         </div>
-        <ul className="navigation__list">
+        <div className="navigation__mobile-menu">
+          <button
+            onClick={(opened) => handleClick(opened)}
+            className="navigation__hamburger"
+          ></button>
+        </div>
+        <ul
+          className={
+            opened
+              ? 'navigation__list'
+              : 'navigation__list navigation__list--closed '
+          }
+        >
           <Link to="/about" className="navigation__link">
             <li className="navigation__item">O nás</li>
           </Link>
